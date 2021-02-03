1c1ab57da43e023c69d1adc3f46991d77e25281d
Ruveera <ruveera3000@gmail.com>
2021-02-03 13:35:04 +0530

    first version

---
+++ src/main/java/com/example/demo/registration/RegistrationService.java
@@ -0,0 +1,39 @@
+package com.example.demo.registration;
+
+import com.example.demo.appuser.AppUser;
+import com.example.demo.appuser.AppUserRole;
+import com.example.demo.appuser.AppUserService;
+import com.example.demo.registration.token.ConfirmationToken;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+
+
+@Service
+@AllArgsConstructor
+public class RegistrationService {
+
+    private final AppUserService appUserService;
+    private final EmailValidator emailValidator;
+
+    public String register(RegistrationRequest request) {
+        boolean isValidEmail = emailValidator.test(request
+                .getEmail());
+        if (!isValidEmail) {
+            throw new IllegalStateException("email not valid");
+        }
+        return appUserService.signUpUser(
+                new AppUser(
+                        request.getFirstName(),
+                        request.getLastName(),
+                        request.getEmail(),
+                        request.getPassword(),
+                        AppUserRole.USER
+                )
+        );
+
+    }
+
+}
